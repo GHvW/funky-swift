5e95e4612bb647b656edf77a866d41ec2783be23
Garrett van Wageningen <ghvanw283@gmail.com>
2019-02-10 10:05:46 -0600

    fix bind and kleisli

--- Sources/funky-swift/funky_swift.swift
+++ Sources/funky-swift/funky_swift.swift
@@ -20,56 +20,37 @@ public func |> <T, U>(val: T, fn: (T) -> U) -> U {
     return fn(val)
 }
 
-// precedencegroup Bind {
-//     associativity: left
-//     higherThan: TernaryPrecedence
-// }
 
-//Bind
+// Bind, >>=
 infix operator ||> : ForwardPipe
 
-public func ||> <T, E, U>(val: Result<T, E>, fn: (T) -> U) -> Result<U, E> {
-    switch val {
-        case .Ok(let x):
-            return .Ok(fn(x))
+public func ||> <T, E, U>(result: Result<T, E>, fn: (T) -> Result<U, E>) -> Result<U, E> {
+    switch result {
+        case .Ok(let data):
+            return fn(data)
         case .Err(let e):
             return .Err(e)
     }
 }
 
-//RocketShip
 precedencegroup Composition {
     associativity: left
     higherThan: ForwardPipe
 }
 
-infix operator >=> : Composition
-
-public func >=> <T, E, U, V>(fn1: @escaping (T) -> Result<U, E>, fn2: @escaping (U) -> V) -> (T) -> Result<V, E> {
-    return { val -> Result<V, E> in 
-        switch fn1(val) {
-            case Result.Ok(let x):
-                return .Ok(fn2(x))
-            case Result.Err(let e):
-                return .Err(e)
-        }
-    }
-}
-
-// precedencegroup SingleComposition {
-//     associativity: left
-//     higherThan: TernaryPrecedence
-// }
-
-//Single Composition
+// Single Composition
 infix operator >-> : Composition
 
 public func >-> <T, U, V>(fn1: @escaping (T) -> U, fn2: @escaping (U) -> V) -> (T) -> V {
     return { val -> V in fn2(fn1(val)) }
 }
 
-// func add(_ x: Int) -> Int { return x + 5 }
 
-// let vv = 5 |> add
+// RocketShip... fine Kleisli
+infix operator >=> : Composition
 
-// print(vv)
+public func >=> <T, E, U, V>(fn1: @escaping (T) -> Result<U, E>, fn2: @escaping (U) -> Result<V, E>) -> (T) -> Result<V, E> {
+    return { val in
+        fn1(val) ||> fn2
+    }
+}
